7a10bdce113422cfe17f86180d1ddf6d1c774713
Etienne Jézéquel <etiennejezequel@MacBook-Pro-de-Etienne.local>
2016-11-08 14:59:57 +0100

    RM *

--- public/script.js
+++
@@ -1,7 +0,0 @@
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-
-document.getElementById("snap").addEventListener("click", function() {
-	context.drawImage(video, 0, 0, 640, 480);
-});
