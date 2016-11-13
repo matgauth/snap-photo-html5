286c26d33eac2a970726124955b61bc540410505
Mathieu Gauthier <mathieu350@gmail.com>
2016-11-13 20:53:07 +0100

    Red sights + service worker (sw-precache) + update Notif

--- public/script.js
+++ public/script.js
@@ -1,183 +1,68 @@
 ﻿// On récupère l'élément container du DOM déjà existant au chargement de la page.
 const video = document.querySelector('video');
-const canvas = document.querySelector('#photo');
-const image = new Image();
+const canvas = document.querySelector('#canvas');
 const reset = document.querySelector('#reset');
 const download = document.querySelector('#download');
 const snap = document.querySelector('#snap');
 const mapButton = document.querySelector('#mapButton');
-const viseur = document.querySelector('#viseur');
+const sights = document.querySelector('#sights');
 const filters = document.querySelector('#filters');
-
+const map = document.querySelector('#map');
+const image = document.createElement('img');
 
 const db = new Dexie("image_database");
 db.version(1).stores({
-   images: 'date,dataURL,lat,long'
+    images: 'date,dataURL,lat,long'
 });
 db.open().catch(function (e) {
-   console.error(e);
+    console.error(e);
 });
 
 // Fonction permettant de vérifier si le navigateur est compatible avec la géolocalisation et de récupérer la position de l'utilisateur
-function getLocation() {
+function getLocation(cb) {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(cb);
     } else {
         console.error("Geolocation not supported !")
     }
 }
-// Fonction permettant d'afficher la position de l'utilisateur
-function showPosition(position) {
-    const formatDate = new Date(position.timestamp).toLocaleString(navigator.language);
-    storeImage(position.coords.latitude, position.coords.longitude, formatDate);
-}
 
-
-function storeImage(lat, long, date) {
+function storeImage(position) {
     const dataURL = canvas.toDataURL('image/png');
-    db.images.put({date, lat, long, dataURL}).catch(function(error) {
-       console.log(error);
+    const date = new Date(position.timestamp).toLocaleString(navigator.language);
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    db.images.put({ date, lat, long, dataURL }).catch(function (error) {
+        console.log(error);
     });
 }
 
-function initApp() {
-            const contextViseur = viseur.getContext('2d');
-            var centerY = viseur.height / 2;
-            var centerX = viseur.width / 2;
-            var radius = 20;
-
-            contextViseur.beginPath();
-            contextViseur.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-            contextViseur.stroke();
-
-            const context = canvas.getContext('2d');
-
-            snap.addEventListener("click", function() {
-	            context.drawImage(video, 0, 0, 640, 480);
-
-                getLocation();
-
-                Notification.requestPermission().then(function(permission){
-                    if (permission=="granted"){
-                        new Notification("Nouvelle photo !",  {
-                            body: "Nouvelle photo ajoutée sur la carte"
-                        });
-                    }
-                })
-
-                reset.removeAttribute('hidden');
-                download.removeAttribute('hidden');
-                snap.setAttribute('hidden', true);
-                viseur.setAttribute('hidden', true);
-                reglage.removeAttribute('hidden');
-                const canvasBackup = canvas.toDataURL('image/png');
-                image.src = canvasBackup;
-                console.log("Capture done !");
+function notificationPermission() {
+    Notification.requestPermission().then(function (permission) {
+        if (permission == "granted") {
+            new Notification("Nouvelle photo !", {
+                icon: './notif.png',
+                body: "Vous avez ajouté un marqueur sur la carte."
             });
-
-            mapButton.addEventListener("click", function(e){
-                if (map.hasAttribute('hidden')) {
-                    map.removeAttribute('hidden');
-                    viseur.setAttribute('hidden', true);
-                    initMap();
-                } else {
-                    map.setAttribute('hidden', true);
-                    viseur.removeAttribute('hidden');
-                }
-            });
-            download.addEventListener('click', function (e) {
-                const dataURL = canvas.toDataURL('image/png');
-	            download.download = "photo.png";
-                download.href = dataURL;
-                console.log("Downloading...")
-            });
-
-            reset.addEventListener('click', function(e) {
-                resetFilters();
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                reset.setAttribute('hidden', true);
-                download.setAttribute('hidden', true);
-                filters.setAttribute('hidden',true);
-                reglage.setAttribute('hidden', true);
-                viseur.removeAttribute('hidden');
-                snap.removeAttribute('hidden');
-                console.log("reset !");
-            });
-
-            reglage.addEventListener('click', function(e) {
-                if (filters.hasAttribute('hidden')) {
-                    filters.removeAttribute('hidden');
-                } else {
-                    filters.setAttribute('hidden', true);
-                }
-            });
-
-            const mouse = {x: 0, y: 0};
-            context.lineWidth = 5;
-            context.lineJoin = 'round';
-            context.lineCap = 'round';
-            context.strokeStyle = 'blue';
-
-            canvas.addEventListener('mousemove', function(e) {
-                mouse.x = e.pageX - this.offsetLeft;
-                mouse.y = e.pageY - this.offsetTop;
-            }, false);
-
-            canvas.addEventListener('mousedown', function(e) {
-                context.beginPath();
-                context.moveTo(mouse.x, mouse.y);
-                canvas.addEventListener('mousemove', onPaint, false);
-            }, false);
-
-            canvas.addEventListener('mouseup', function(e) {
-                canvas.removeEventListener('mousemove', onPaint, false);
-            }, false);
-
-            const onPaint = function() {
-                context.lineTo(mouse.x, mouse.y);
-                context.stroke();
-            };
-
-            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                navigator.mediaDevices.getUserMedia({video: true}).then(function(stream){
-                    video.src = window.URL.createObjectURL(stream);
-                    video.play();
-                });
-            } else {
-                console.error("Your device can't use navigator.mediaDevices");
-            }
-      }
-
-window.onload = function() {
-    initApp();
-};
-
-function updateSlider()
-{
-    if(document.getElementById('invert').checked){
-        var invert = 100;
-    } else {
-        var invert = 0;
-    }
-    if(document.getElementById('sepia').checked){
-        var sepia = 100;
-    } else {
-        var sepia = 0;
-    }
-    if(document.getElementById('grayscale').checked){
-        var grayscale = 100;
-    } else {
-        var grayscale = 0;
-    }
-    var saturate = document.getElementById('saturate').value;
-    var brightness = document.getElementById('brightness').value;
-
-    const context = canvas.getContext('2d');
-    context.filter = "invert("+invert+") sepia("+sepia+") saturate("+saturate+") brightness("+brightness+") grayscale("+grayscale+")";
-    context.drawImage(image, 0, 0, 640, 480);
+        }
+    })
 }
 
-function resetFilters(){
+function updateSlider(e) {
+    if (e.target !== e.currentTarget) {
+        let invert = document.getElementById('invert').checked ? 100 : 0;
+        let sepia = document.getElementById('sepia').checked ? 100 : 0;
+        let grayscale = document.getElementById('grayscale').checked ? 100 : 0;
+        let saturate = document.getElementById('saturate').value;
+        let brightness = document.getElementById('brightness').value;
+
+        canvas.getContext('2d').filter = `invert(${invert}) sepia(${sepia}) saturate(${saturate}) brightness(${brightness}) grayscale(${grayscale})`;
+        canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height);
+    }
+    e.stopPropagation();
+}
+
+function resetFilters() {
     document.getElementById('saturate').value = 1;
     document.getElementById('brightness').value = 1;
     document.getElementById('grayscale').checked = false;
@@ -185,30 +70,159 @@ function resetFilters(){
     document.getElementById('sepia').checked = false;
 }
 
-function initMap(){
-    var myLatLng = {lat: 48.41, lng: -4.49};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        scrollwheel: false,
-        zoom: 12
-    });
-    var collection = db.images;
-    collection.each(function(image) {
-        var myLatLng = {lat: Number(image.lat), lng: Number(image.long)};
-        var marker = new google.maps.Marker({
-            map: map,
-            draggable: true,
-            animation: google.maps.Animation.DROP,
-            position: myLatLng
+function initMap() {
+    const collection = db.images;
+    getLocation(function (position) {
+        const myLatLng = { lat: position.coords.latitude, lng: position.coords.longitude };
+        const gmap = new google.maps.Map(map, {
+            center: myLatLng,
+            scrollwheel: true,
+            zoom: 12
         });
-        var infowindow = new google.maps.InfoWindow({
-            content: `
+        collection.each(function (image) {
+            const position = { lat: Number(image.lat), lng: Number(image.long) };
+            const marker = new google.maps.Marker({
+                map: gmap,
+                draggable: true,
+                animation: google.maps.Animation.DROP,
+                position
+            });
+            const infowindow = new google.maps.InfoWindow({
+                content: `
                     <p>Votre photo le ${image.date}</p>
                     <img width="150px" alt="votre photo" src="${image.dataURL}"/>
                     `
-        });
-        marker.addListener('click', function() {
-            infowindow.open(map, marker);
+            });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
         });
     });
- }
+}
+
+function onClickSnap() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+    getLocation(storeImage);
+    notificationPermission();
+
+    reset.removeAttribute('hidden');
+    download.removeAttribute('hidden');
+    canvas.removeAttribute('hidden');
+    snap.setAttribute('hidden', true);
+    sights.setAttribute('hidden', true);
+    reglage.removeAttribute('hidden');
+
+    const data = canvas.toDataURL('image/png');
+    image.setAttribute('src', data);
+    console.log("Capture done !");
+}
+
+function showHideMap() {
+    if (map.hasAttribute('hidden')) {
+        initMap();
+        map.removeAttribute('hidden');
+        video.setAttribute('hidden', true);
+        sights.setAttribute('hidden', true);
+        canvas.setAttribute('hidden', true);
+    } else {
+        map.setAttribute('hidden', true);
+        sights.removeAttribute('hidden');
+        video.removeAttribute('hidden');
+        canvas.removeAttribute('hidden');
+    }
+}
+
+function onClickReset() {
+    resetFilters();
+    canvas.getContext('2d').clearRect(0, 0, video.videoWidth, video.videoHeight);
+    reset.setAttribute('hidden', true);
+    canvas.setAttribute('hidden', true);
+    download.setAttribute('hidden', true);
+    filters.setAttribute('hidden', true);
+    reglage.setAttribute('hidden', true);
+    sights.removeAttribute('hidden');
+    snap.removeAttribute('hidden');
+    console.log("reset !");
+}
+
+function onClickDownload() {
+    const dataURL = canvas.toDataURL('image/png');
+    download.download = "photo.png";
+    download.href = dataURL;
+    console.log("Downloading...");
+}
+
+function showHideSettings() {
+    filters.hasAttribute('hidden') ? filters.removeAttribute('hidden') : filters.setAttribute('hidden', true);
+}
+
+function addSights() {
+    const image = new Image();
+    image.src = 'http://www.earrelaphant.com/wp-content/uploads/2016/03/sniper-297661_640-218x218.png';
+    image.onload = function () {
+        sights.getContext('2d').drawImage(image,
+            sights.width / 2 - image.width / 2,
+            sights.height / 2 - image.height / 2
+        );
+    }
+}
+
+function initApp() {
+    addSights();
+
+    snap.addEventListener("click", onClickSnap, false);
+
+    mapButton.addEventListener("click", showHideMap, false);
+
+    download.addEventListener('click', onClickDownload, false);
+
+    reset.addEventListener('click', onClickReset, false);
+
+    reglage.addEventListener('click', showHideSettings, false);
+
+    filters.addEventListener('change', updateSlider, false);
+
+    const mouse = { x: 0, y: 0 };
+    const context = canvas.getContext('2d');
+    context.lineWidth = 5;
+    context.lineJoin = 'round';
+    context.lineCap = 'round';
+    context.strokeStyle = 'blue';
+
+    canvas.addEventListener('mousemove', function (e) {
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    }, false);
+
+    canvas.addEventListener('mousedown', function (e) {
+        context.beginPath();
+        context.moveTo(mouse.x, mouse.y);
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
+
+    canvas.addEventListener('mouseup', function (e) {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+
+    const onPaint = function () {
+        context.lineTo(mouse.x, mouse.y);
+        context.stroke();
+    };
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        });
+    } else {
+        console.error("Your device can't use navigator.mediaDevices");
+    }
+}
+
+window.onload = function () {
+    initApp();
+};
+
